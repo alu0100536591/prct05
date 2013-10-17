108291e916e1529261c609ad8644af9b3eb73338
Jorge O. Blanchard Cruz <rothgard@rothgard-VirtualBox.(none)>
2013-10-17 18:35:59 +0000

    1º Implementado: initialize, minimize y to_s

--- racional.rb
+++ racional.rb
@@ -3,4 +3,22 @@
 require "./gcd.rb"
 
 class Fraccion
+	def initialize(n,d)
+		@_num, @_den = n, d
+		minimize()
+	end
+	
+	def minimize
+		gcd = gcd(@_num, @_den)
+		@_num /= gcd
+		@_den /= gcd
+	end
+	
+	def to_s
+		"#{@_num} / #{@_den}"
+	end
 end
+
+f1 = Fraccion.new(20,5)
+puts f1
+
